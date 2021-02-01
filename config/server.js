ec3552d5ffe1b0c5b7709b8d828eac883480c7d0
Mikhail Karan <mikhail@contentlinq.com>
2021-02-01 11:09:40 -0500

    inital backend setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '89271909d57ee413d5d14093d0b86d3b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb3c5460-fa99-44ae-9a31-3275057c1c2e'
+};
