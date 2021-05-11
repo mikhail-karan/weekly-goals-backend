4920abefe26f044ca26c652b01980a7999b889de
Mikhail Karan <mikhail@contentlinq.com>
2021-05-11 12:27:24 -0400

    feature: added custom route for active goals

--- api/weekly-goals/controllers/weekly-goals.js
+++ api/weekly-goals/controllers/weekly-goals.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async activeGoals(ctx){
+    console.log('return active goals', ctx.query)
+
+    let ourDate = new Date();
+    const pastDate = ourDate.getDate() - 8;
+    ourDate.setDate(pastDate);
+
+    //Log the date to our web console.
+    // console.log(ourDate);
+
+
+
+    const _goals = strapi.query('weekly-goals').find({Created_gt:ourDate, Done_ne:true, _sort:'Created:asc'})
+    return _goals
+  }
+};
